a878f4bf1bf37a61fee214899258b77103f66ef8
qq <qq@Vaibhav>
2021-03-08 19:16:21 +0530

    This is My second Commit

--- CucumberProject/src/test/java/com/stepDefinations/TestRunner.java
+++ CucumberProject/src/test/java/com/stepDefinations/TestRunner.java
@@ -1,18 +1,16 @@
 package com.stepDefinations;
 
-import org.testng.annotations.DataProvider;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(
-features= {"G:\\Core java\\CucumberProject\\src\\test\\java\\Features\\Login.feature"},
+features= {"E:\\JavaWithSelenium\\-JavaWithSelenium\\CucumberProject\\src\\test\\java\\Features\\Login.feature"},
 glue= {"Features"},strict=true, dryRun=false,monochrome=true,
 plugin= {"pretty","html:targetReports/cucumberReports/cucumber-reports",
-		"json:targetReports/cucumberReports/cucumbert-reports.json","rerun:tagetfailed/failestestcases.txt"}
+		"json:targetReports/cucumberReports/cucumbert-reports.json","rerun:tagetfailed/failestestcases.txt"})
 //tags={"@smoke"}
-)
+
 public class TestRunner  extends AbstractTestNGCucumberTests{
 
 /*	 For execution of multiple feature file and multiple scenarios
